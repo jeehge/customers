7e53e496119bc646a641c2584aa673554197dec1
rednics <rednics@naver.com>
2019-11-11 04:41:23 +0900

    Update organization.sql

--- gsc/organization.sql
+++ gsc/organization.sql
@@ -52,3 +52,16 @@ FROM   tree_query
 ORDER  BY sort 
 
 -------------------------------------------------------------------------------------------------
+CREATE TABLE [pbt].[Talk_Server_Config_Info] (
+  [config_key] varchar(50) NOT NULL,
+  [config_value] varchar(256) NOT NULL,
+  [reg_id] numeric(18)  NOT NULL,
+  [reg_dt] datetime2(7) DEFAULT (getdate()) NOT NULL,
+  [upd_id] numeric(18)  NOT NULL,
+  [upd_dt] datetime2(7) DEFAULT (getdate()) NOT NULL
+)
+GO
+-------------------------------------------------------------------------------------------------				 
+Download-Allow-Code-For.C1	C7,C8	0	2019-11-05 09:53:30.5730000	0	2019-11-05 09:53:30.5730000
+Download-Allow-Code-For.C7	C1,C8	0	2019-11-08 03:26:37.0000000	0	2019-11-08 03:27:35.0000000
+Download-Allow-Code-For.C8	C1,C7	0	2019-11-08 03:27:14.0000000	0	2019-11-08 03:27:18.4930000
